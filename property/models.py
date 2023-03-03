3c6a4d0f4d9924cf22239f968dc8ef9ff95dc052
Aleksei Lobachev <90100576+LexLobachev@users.noreply.github.com>
2023-03-03 21:24:26 +0400

    add db model for complaints

--- property/models.py
+++ property/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Flat(models.Model):
@@ -50,3 +51,16 @@ class Flat(models.Model):
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
+
+
+class Complaint(models.Model):
+    user = models.ForeignKey(
+        User,
+        'Кто жаловался:',
+        null=True)
+    flat = models.ForeignKey(
+        Flat,
+        'Квартира, на которую пожаловались:',
+        null=True)
+    address = models.TextField(
+        'Текст жалобы:')
